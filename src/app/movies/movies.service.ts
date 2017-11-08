e444c0909c655e78d9688bf50e28cff8fa4519ab
Olivier Mungo <olivier.mungo@ec.europa.eu>
2017-11-08 18:18:21 +0100

    Updating MoviesService to use Firebase in RPDO

--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -7,24 +7,93 @@ import { Movie } from './movie.entity';
 
 @Injectable()
 export class MoviesService {
-  constructor(private http: Http) { }
+  private targetCreate: any;
+  private targetRetrieve: any;
+  private targetUpdate: any;
+  private targetDelete: any;
+
+  constructor(private http: Http) {
+    if (environment.production) {
+      this.targetCreate = this.createFirebase;
+      this.targetRetrieve = this.retrieveFirebase;
+      this.targetUpdate = this.updateFirebase;
+      this.targetDelete = this.deleteFirebase;
+    } else {
+      this.targetCreate = this.createLocal;
+      this.targetRetrieve = this.retrieveLocal;
+      this.targetUpdate = this.updateLocal;
+      this.targetDelete = this.deleteLocal;
+    }
+  }
 
   create(movie: Movie): Observable<any> {
+    return this.targetCreate(movie);
+  }
+
+  retrieve(): Observable<Movie[]> {
+    return this.targetRetrieve();
+  }
+
+  update(movie: Movie): Observable<any> {
+    return this.targetUpdate(movie);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.targetDelete(id);
+  }
+
+  createLocal(movie: Movie) {
     const id = new Date().getTime().toString();
     const newMovie = { ...movie, id };
 
     return this.http.post(`${environment.backEnd}/movies`, newMovie).map(() => newMovie);
   }
 
-  retrieve(): Observable<any> {
-    return this.http.get(`${environment.backEnd}/movies`).map(movies => movies.json());
+  createFirebase(movie: Movie) {
+    return this.http.post(`${environment.backEnd}/movies.json`,
+      JSON.stringify({ title: movie.title, genre: movie.genre, year: movie.year, director: movie.director }))
+      .map(movies => movies.json())
+      .map(result => ({ ...movie, id: result.name }));
   }
 
-  update(movie: Movie): Observable<any> {
-    return this.http.put(`${environment.backEnd}/movies/${movie.id}`, movie);
+  retrieveLocal(): Observable<Movie[]> {
+    return this.http.get(`${environment.backEnd}/movies`)
+      .delay(1000)
+      .map(movies => movies.json());
   }
 
-  delete(id: string): Observable<any> {
+  retrieveFirebase(): Observable<Movie[]> {
+    return this.http.get(`${environment.backEnd}/movies.json`)
+      .delay(1000)
+      .map(movies => movies.json())
+      .map(movies => {
+        const moviesArray = [];
+
+        for (const key in movies) {
+          if (movies.hasOwnProperty(key)) {
+            moviesArray.push(<Movie>{ id: key, title: movies[key].title, genre: movies[key].genre, year: movies[key].year, director: movies[key].director });
+          }
+        }
+
+        return moviesArray;
+      });
+  }
+
+  updateLocal(movie: Movie) {
+    return this.http.put(`${environment.backEnd}/movies/${movie.id}`,
+      { title: movie.title, genre: movie.genre, year: movie.year, director: movie.director });
+  }
+
+  updateFirebase(movie: Movie) {
+    return this.http.patch(`${environment.backEnd}/movies/${movie.id}.json`,
+      JSON.stringify({ title: movie.title, genre: movie.genre, year: movie.year, director: movie.director }));
+  }
+
+  deleteLocal(id: string) {
     return this.http.delete(`${environment.backEnd}/movies/${id}`);
+  }
+
+  deleteFirebase(id: string) {
+    return this.http.delete(`${environment.backEnd}/movies/${id}.json`);
   }
 }
